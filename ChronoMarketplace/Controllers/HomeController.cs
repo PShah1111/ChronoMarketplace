99d5b7395fbd6e5891b026651cdcd9a166cf5454
Parin Shah <ac116525@avcol.school.nz>
2024-04-04 18:25:40 +1300

    Pages attached for Buy a Watch & Sell a Watch dropdowns in the navbar.

--- ChronoMarketplace/Controllers/HomeController.cs
+++ ChronoMarketplace/Controllers/HomeController.cs
@@ -26,6 +26,32 @@ namespace ChronoMarketplace.Controllers
         {
             return View();
         }
+        public IActionResult AllWatches()
+        {
+            return View();
+        }
+        public IActionResult Brands()
+        {
+            return View();
+        }
+        public IActionResult Accessories()
+        {
+            return View();
+        }
+        public IActionResult SellNow()
+        {
+            return View();
+        }
+        public IActionResult SellingForm()
+        {
+            return View();
+        }
+        public IActionResult TermsandConditions()
+        {
+            return View();
+        }
+
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
